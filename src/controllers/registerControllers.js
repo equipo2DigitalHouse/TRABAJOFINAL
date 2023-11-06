27c150ac219561626e9224bab0c9e158ec38c81a
Martin <martinassell@gmail.com>
2023-11-05 18:32:49 -1200

    Modificando el registro

--- src/controllers/registerControllers.js
+++ src/controllers/registerControllers.js
@@ -5,15 +5,18 @@ const registerController = {
         res.render(path.join(__dirname,"../views/register"))
     }
 ,
-    processRegister: (req, res) => {
-        const resultValidation= validationResult(req);
+    processRegister: (req, res) => { //este procesa la solicitud de registro
+        const resultValidation= validationResult(req); 
+        return res.send (resultValidation.mapped())
 
-        if (resultValidation.errors.length >0) {
-            return res.render ('register', {
+
+        if (resultValidation.errors.length > 0) {
+            return res.render(path.join(__dirname, "../views/register"), {
                 errors: resultValidation.mapped()
-            })
+            });
         }
-
+        User.create (req.body);
+        return res.render ('Ok. Se guardó el usuario')
     },
     login: (req, res) => {
         
