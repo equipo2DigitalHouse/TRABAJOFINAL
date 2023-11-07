b3a996bb860b3fc351d8608fb72782dcdf9ec8f8
Santiago <santiagoruth1704@gmail.com>
2023-11-07 11:27:05 -0300

    cookies and login

--- src/controllers/loginControllers.js
+++ src/controllers/loginControllers.js
@@ -1,8 +1,48 @@
 const path = require("path");
+const { check, validationResult, body } = require("express-validator");
+const userModule = require("../database/users.json");
+const express = require("express");
+const router = express.Router();
+
 const loginController = {
-    login: (req, res) =>{
-        res.render(path.join(__dirname,"../views/login"))
+    login: (req, res) => {
+        res.render(path.join(__dirname, "../views/login"), { errors: [] });;
+    },
+    processLogin: (req, res) => {
+        let errors = validationResult(req);
+        let users = userModule; 
+
+        // console.log('users:', users); 
+        // console.log('req.body:', req.body); 
+
+        let usuarioALoguearse;
+
+        if (errors.isEmpty()){
+            for (let i = 0; i < users.length; i++) {
+                if (users[i].email == req.body.email && users[i].password == req.body.password) {
+                    usuarioALoguearse = users[i];
+                    break;
+                }
+            }
+        }
+
+        // console.log('usuarioALoguearse:', usuarioALoguearse); 
+
+        if (usuarioALoguearse === undefined) {
+            return res.render(path.join(__dirname, "../views/login"), { errors: [{ msg: "Credenciales inválidas" }] },);
+        } else {
+            req.session.usuarioLogedo = usuarioALoguearse;
+            res.send("el ususario logueado es "+ req.session.usuarioLogedo.email), { errors: errors.array() };
+        }
     }
-}
+};
+
+// router.get("/check"),(req,res) => {
+//     if(req.session.usuarioLogedo == undefined){
+//         res.send("el usuario no esta logueado")
+//     }else{
+//         res.send("el ususario logueado es "+ req.session.usuarioLogedo.email);
+//     }
+// }
 
 module.exports = loginController;
--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const loginController = require("../controllers/loginControllers");
-router.get("/login", loginController.login)
+const { check } = require("express-validator");
+
+router.get("/login",[
+    check("email").isEmail().withMessage("Email invalido"),
+    check("password").isLength({min: 8}).withMessage("la contraseña debe tener al menos 8 caracteres")
+] ,loginController.login);
+
+router.post("/login",loginController.processLogin)
 
 module.exports = router;
