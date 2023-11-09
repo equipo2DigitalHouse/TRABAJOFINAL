c5e6c14fd566a527f195bd9078c05be03dc879be
Santiago <santiagoruth1704@gmail.com>
2023-11-08 23:42:11 -0300

    home_error_login

---
+++ src/middlewares/authLogin.js
@@ -0,0 +1,31 @@
+
+
+const authLogin = (req,res,next) => {
+
+    const express = require('express');
+    const app = express();
+    
+}
+
+//     const open = document.getElementById("open");
+//     const containerpopup = document.getElementById("container-pop-up");
+//     const close = document.getElementById("close");
+    
+    // open.addEventListener("click", () => {
+    //     containerpopup.classList.add("show");
+    // })
+    
+//     close.addEventListener("click", () => {
+//         containerpopup.classList.remove("show");
+    // })
+    
+    // next();
+// }
+
+module.exports = authLogin;
+
+
+
+
+
+
--- src/routes/listRoutes.js
+++ src/routes/listRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
+// const authLogin = require("../middlewares/authLogin")
 const listController = require("../controllers/listControllers");
-router.get("/list", listController.listado)
+router.get("/list",listController.listado)
 
 module.exports = router;
