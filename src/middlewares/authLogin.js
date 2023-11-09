038740da10f8f8e8a5c9243c3d1f7d4b7cd31d43
mapi bertinat <santiagoruth1704@gmail.com>
2023-11-09 12:43:51 -0300

    login_logic_sucessfull/adding_css_for_home

--- src/middlewares/authLogin.js
+++ src/middlewares/authLogin.js
@@ -5,22 +5,13 @@ const authLogin = (req,res,next) => {
     const express = require('express');
     const app = express();
     
-}
+    if(req.session.usuarioLogedo == undefined){
+        res.redirect("../home_login_error")
+    }else{
+        next();
+    }
+};
 
-//     const open = document.getElementById("open");
-//     const containerpopup = document.getElementById("container-pop-up");
-//     const close = document.getElementById("close");
-    
-    // open.addEventListener("click", () => {
-    //     containerpopup.classList.add("show");
-    // })
-    
-//     close.addEventListener("click", () => {
-//         containerpopup.classList.remove("show");
-    // })
-    
-    // next();
-// }
 
 module.exports = authLogin;
 
