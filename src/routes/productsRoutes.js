3ec9863a68d24149eb4eb4f43d1b461e575134d1
Santiago <santiagoruth1704@gmail.com>
2023-11-11 20:22:20 -0300

    ModifyProducts

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,15 +1,22 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 const productsController = require("../controllers/productsControllers");
 
+const adminCheck =require("../middlewares/admincheck");
+const confirmModifyProduct = require("../controllers/confirmModifyProduct");
 const getAllProducts = require("../controllers/getAllProducts");
 const deleteProduct = require("../controllers/deleteProductController");
+const getProductById = require("../controllers/getProductById");
 
-// const {getAllProducts} = require("../controllers/")
+// const { getAllProducts , getProductById, deleteProduct, productsController } = require("../controllers");
 
 router.get("/", productsController.products);
 router.get("/products",getAllProducts);
 
+router.get("/product/:id",adminCheck,getProductById);
+
 router.delete('/product/delete/:id', deleteProduct);
 
+router.put("/product/edit/:id",confirmModifyProduct);
 module.exports = router;
