f59f94ac11f90d9d74cddddfb96be73d83155f0c
Martin <martinassell@gmail.com>
2023-12-10 16:21:22 -1200

    agrego validaciones de usuarios y arregle el boton de enviar del Register

--- src/routes/registerRoutes.js
+++ src/routes/registerRoutes.js
@@ -19,32 +19,31 @@ const uploadFile= multer ({ storage });
 const { body } = require ('express-validator') //tambièn se la conoce como variable check, son lo mismo, pero body
 // es mas asociable a la idea de validar el formulario
 
+ const validations= [
+body('name').notEmpty().withMessage('Escribe tu nombre'),
+body('surname').notEmpty().withMessage('Escribe tu apellido'),
+body('Email').notEmpty().withMessage('Escribe tu Email').bail()
+.isEmail().withMessage('Debes escribir un formato de correo electrónico válido'),
+body('userName').notEmpty().withMessage('Escribe tu nombre de usuario'),
+body('avatar').notEmpty().withMessage('Selecciona tu foto de perfil'),
+body('password').notEmpty().withMessage('Escribe tu contraseña'),
+body('avatar').custom((value, { req }) => {
+let file = req.file;
+      
 
+if (!file) {
+throw new Error ('Tienes que cargar una imagen');
+}
+return true;
+})
+]
 
 
 const registerController = require("../controllers/registerControllers");
 
 router.get("/register", registerController.register)
 //ruta de posteo para la suba de imagen de perfil 
-router.post("/register",uploadFile.single("avatar"),registerController.processRegister) 
+router.post("/register",uploadFile.single("avatar"),validations, registerController.processRegister) 
 //lleva avatar xq es el name del formulario de registro
 
 module.exports = router;
-// const validations= [
-//     body('name').notEmpty().withMessage('Escribe tu nombre'),
-//     body('surname').notEmpty().withMessage('Escribe tu apellido'),
-//     body('Email').notEmpty().withMessage('Escribe tu Email').bail()
-//     .isEmail().withMessage('Debes escribir un formato de correo electrónico válido'),
-//     body('userName').notEmpty().withMessage('Escribe tu nombre de usuario'),
-//     body('avatar').notEmpty().withMessage('Selecciona tu foto de perfil'),
-//     body('password').notEmpty().withMessage('Escribe tu contraseña'),
-//     body('avatar').custom((value, { req }) => {
-//       let file = req.file;
-      
-
-//       if (!file) {
-//         throw new Error ('Tienes que cargar una imagen');
-//       }
-//       return true;
-//     })
-// ]
