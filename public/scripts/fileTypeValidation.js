b474ca80003fb75b47c0264df8850caf65039934
Santiago <santiagoruth1704@gmail.com>
2023-12-20 21:00:55 -0300

    solving img validation register/adminform

--- public/scripts/fileTypeValidation.js
+++ public/scripts/fileTypeValidation.js
@@ -1,14 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-        document.querySelector('form').addEventListener('submit', event => {
-            const fileInput = document.getElementById('img_validation');
-            const errorMessage = document.getElementById('error-message');
-            
-            if (fileInput.files.length === 0 || !/\.(jpg|jpeg|png|gif)$/i.test(fileInput.files[0].name)) {
-                errorMessage.style.display = 'block';
-                event.preventDefault();
-            }
-        });
+    document.querySelector('form').addEventListener('submit', event => {
+        const fileInput = document.getElementById('img_validation');
+        const errorMessage = document.getElementById('error-message');
+
+        if (fileInput.files.length === 0) {
+
+            return;
+        }
+
+        if (!/\.(jpg|jpeg|png|gif)$/i.test(fileInput.files[0].name)) {
+            errorMessage.style.display = 'block';
+            event.preventDefault();
+        }
     });
+});
 
 
 // document.addEventListener("DOMContentLoaded", function () {
