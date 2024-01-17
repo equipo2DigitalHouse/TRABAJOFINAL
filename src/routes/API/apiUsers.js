e117eed5acd339d8827ebff7c72441b865fa4216
Java7755 <javier.cristani@gmail.com>
2024-01-17 20:59:08 -0300

    agregando cambios

--- src/routes/API/apiUsers.js
+++ src/routes/API/apiUsers.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const apiUsersControllers = require('../controllers/api/apiUsersControllers');
+const apiUsersControllers = require('../../controllers/api/apiUsersControllers');
 
 //Todos los productos//
-router.get('/', apiUsersControllers.index);
+router.get('/users', apiUsersControllers.index);
 
 
 //Detalle de producto//
