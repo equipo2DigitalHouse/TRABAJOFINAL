a28ffc4af121a98edc0f15cc9ec0d90b7d5ed75a
Santiago <santiagoruth1704@gmail.com>
2024-01-29 17:00:04 -0300

    solving css at index

--- src/js/shop.js
+++
@@ -1,9 +0,0 @@
-
-let select= prompt("Hola! ¿Necesitas consejos para planificar tu fiesta? Un asesor se puede comunicar con vos...")
-
-while (select !="si" && select != "no") {
-    alert ("Por favor, ingresa si o no desde el teclado")
-    select = prompt ("Para poder realizar compras, debés estár registrado... ")
-}
-
-module.exports = select
